46c190d6c36089d9b7c5b44ce224e7843371a9a3
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2026-09-21 12:56:59 +0200

    use enum

--- tests/ZweiFaktorAuthEinrichten.spec.ts
+++ tests/ZweiFaktorAuthEinrichten.spec.ts
@@ -122,7 +122,7 @@ test.describe('Zwei-Faktor-Authentifizierung als Admin einrichten', () => {
         rollenname,
         undefined,
         undefined,
-        new Set([await getServiceProviderId(page, itslearning, schuleId, 'LERN')]),
+        new Set([await getServiceProviderId(page, itslearning, schuleId, RollenArt.Lern)]),
       );
       const userInfo: UserInfo = await createPerson(page, {
         organisationId: schuleId,
